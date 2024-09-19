f45f81a296d1a3d963f2f9a27d8d11ef7297abec
Todd Demone <tdemone1@gmail.com>
2024-09-19 14:40:28 -0400

    clean up list of lint providers (installed markdownlint with Mason)

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -5,15 +5,15 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-        python = { 'ruff' }, -- add 'mypy' later
-      }
+      --      lint.linters_by_ft = {
+      --        markdown = { 'markdownlint' },
+      --      }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+      lint.linters_by_ft = lint.linters_by_ft or {}
+      lint.linters_by_ft['markdown'] = { 'markdownlint' }
+      lint.linters_by_ft['python'] = { 'ruff' }
       --
       -- However, note that this will enable a set of default linters,
       -- which will cause errors unless these tools are available:
